941587d947a6f12f275ea574e4eb19b7e0273e73
RobertoMay <7630@itescam.edu.mx>
2024-01-18 12:29:52 -0600

    Added playRound and game functions

--- index.js
+++ index.js
@@ -8,7 +8,70 @@ function getComputerChoice() {
     } else if (x === 3) {
         return 'Scissors';
     }
-    
+
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    if (player === 'rock' && computer === 'scissors') {
+        return "You Win! Rock beats Scissors"
+    } else if (player === 'rock' && computer === 'paper') {
+        return "You Lose! Paper beats Rock"
+    } else if (player === 'paper' && computer === 'rock') {
+        return "You Win! Paper beats Rock"
+    } else if (player === 'paper' && computer === 'scissors') {
+        return "You Lose! Scissors beat Paper"
+    } else if (player === 'scissors' && computer === 'paper') {
+        return "You Win! Scissors beat Paper"
+    } else if (player === 'scissors' && computer === 'rock') {
+        return "You Lose! Rock beats Scissors"
+    } else if (player === computer) {
+        return "TIE play again"
+    } else {
+        return "Enter a valid value"
+    }
+}
+
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    for (let index = 0; index < 5; index++) {
+        let player = prompt('Select between Rock, Paper or Scissors: ');
+        let round = playRound(player, getComputerChoice());
+        let winOrLose = round.slice(0, 5);
+
+        if (winOrLose === 'You W') {
+            console.log(round);
+            scorePlayer++;
+            console.log("You: " + scorePlayer + "  Computer: " + scoreComputer);
+        } else if (winOrLose === 'You L') {
+            console.log(round);
+            scoreComputer++;
+            console.log("You: " + scorePlayer + "  Computer: " + scoreComputer);
+        } else if (winOrLose === 'TIE p') {
+            console.log(round);
+            console.log("You: " + scorePlayer + "  Computer: " + scoreComputer);
+        } else {
+            console.log(round);
+            break;
+        }
+    }
+
+    if (scorePlayer > scoreComputer) {
+        console.log("");
+        console.log("You Win!!!");
+        console.log("You: " + scorePlayer + "  Computer: " + scoreComputer);
+    } else if (scorePlayer === scoreComputer) {
+        console.log("TIE");
+    } else {
+        console.log();
+        console.log("You Lose!");
+        console.log("You: " + scorePlayer + "  Computer: " + scoreComputer);
+    }
+
+}
+
+game();
